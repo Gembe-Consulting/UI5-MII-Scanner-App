3df9e17e15832ee871333b3cf39b937e8db1d9bf
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2017-11-24 10:23:31 +0000

    Add goodsReceipt controller

---
+++ webapp/controller/action/GoodsReceipt.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"./ActionBaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox",
+	"sap/m/MessageToast"
+], function(ActionBaseController, JSONModel, MessageBox, MessageToast) {
+	"use strict";
+
+	return ActionBaseController.extend("com.mii.scanner.controller.action.GoodsReceipt", {
+		onInit:function(){
+			
+		}
+	});
+
+});
