26dceba0d24433732bbf0417000039961624c47a
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2017-11-24 12:44:30 +0000

    Update resourceRoots

--- webapp/controller/action/ActionBaseController.js
+++ webapp/controller/action/ActionBaseController.js
@@ -1,36 +1,71 @@
 sap.ui.define([
 	"com/mii/scanner/controller/PageBaseController",
 	"sap/m/MessageBox",
-	"sap/m/MessageToast"
-], function(PageBaseController, MessageBox, MessageToast) {
+	"sap/m/MessageItem",
+	"sap/m/MessagePopover",
+	"sap/m/MessageToast",
+	"sap/ui/core/message/ControlMessageProcessor",
+	"openui5/validator/Validator"
+], function(PageBaseController, MessageBox, MessageItem, MessagePopover, MessageToast, ControlMessageProcessor, Validator) {
 	"use strict";
 
 	return PageBaseController.extend("com.mii.scanner.controller.action.ActionBaseController", {
+		_initMessageManager: function() {
+			//Initialize the message processor and message manager
+			this._messageManager = sap.ui.getCore().getMessageManager();
+			this._messageManager.registerMessageProcessor(new ControlMessageProcessor());
 
-		onSave:function(){
+			//Initialize the Message Popover used to display the errors
+			this._messagePopover = new MessagePopover({
+				items: {
+					path: 'message>',
+					template: new MessageItem({
+						description: '{message>description}',
+						type: '{message>type}',
+						title: '{message>message}',
+						subtitle: 'ID adssa'
+					})
+				}
+			});
+		},
+
+		_onValidationSuccess: function() {
+			this._messagePopover.close();
+			this.getView().byId('btMessagePopover').setVisible(false);
+			MessageToast.show('Form is valid! No errors!');
+		},
+
+		_onValidationError: function(validationResult) {
+			this._messageManager.addMessages(validationResult.ui5ErrorMessageObjects);
+			this.getView().byId('btMessagePopover').setText(validationResult.ui5ErrorMessageObjects.length);
+			this.getView().byId('btMessagePopover').setVisible(true);
+			MessageBox.error('Form is invalid! It contains errors!', {
+				details: validationResult.originalErrorMessages
+			});
+		},
+		onSave: function() {
 			MessageToast.show(this.getResourceText("messageSuccessBaseActionController"));
 			this.onNavBack();
 		},
-		
+
 		onCancelAction: function(oEvent) {
 			this.handleConfirmationMessageBoxPress(oEvent);
 		},
-		
+
 		handleConfirmationMessageBoxPress: function(oEvent) {
 			var sMessage = this.getResourceText("messageConfirmCancelBaseActionController"),
 				sTitle = this.getResourceText("titleConfirmCancelBaseActionController");
-				
+
 			MessageBox.confirm(sMessage, {
-					title: sTitle,
-					actions: [MessageBox.Action.YES, MessageBox.Action.NO],
-					defaultAction: MessageBox.Action.NO,
-					styleClass: this._bCompact ? "sapUiSizeCompact" : "",
-					onClose: this.fnConfirmCancelAction.bind(this)
-				}
-			);
-			
+				title: sTitle,
+				actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+				defaultAction: MessageBox.Action.NO,
+				styleClass: this._bCompact ? "sapUiSizeCompact" : "",
+				onClose: this.fnConfirmCancelAction.bind(this)
+			});
+
 		},
-		
+
 		fnConfirmCancelAction: function(oAction) {
 			if (oAction === sap.m.MessageBox.Action.YES) {
 				this.onNavBack();
--- webapp/libs/openui5/validator/library.js
+++ webapp/libs/openui5/validator/library.js
@@ -1,25 +1,26 @@
-sap.ui.define([], function() {
-	'use strict';
+sap.ui.define([
+], function() {
+  'use strict';
 
-	/**
-	 * OpenUI5 library: openui5.validator.
-	 *
-	 * @namespace
-	 * @name openui5.validator
-	 * @author Mauricio Lauffer
-	 * @version ${version}
-	 * @public
-	 */
-	// delegate further initialization of this library to the Core
-	var validator = sap.ui.getCore().initLibrary({
-		name: 'openui5.validator',
-		dependencies: [
-			'sap.ui.core'
-		],
-		controls: ['openui5.validator.Validator'],
-		noLibraryCSS: true,
-		version: '${version}'
-	});
+  /**
+   * OpenUI5 library: openui5.validator.
+   *
+   * @namespace
+   * @name openui5.validator
+   * @author Mauricio Lauffer
+   * @version ${version}
+   * @public
+   */
+  // delegate further initialization of this library to the Core
+  var validator = sap.ui.getCore().initLibrary({
+    name: 'openui5.validator',
+    dependencies: [
+      'sap.ui.core'
+    ],
+    controls: ['openui5.validator.Validator'],
+    noLibraryCSS: true,
+    version: '${version}'
+  });
 
-	return validator;
+  return validator;
 }, /* bExport= */ false);
