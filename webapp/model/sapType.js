186dbd4298ee960229436b25a80bb984b6621155
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2017-11-26 20:01:39 +0000

    Introduce SAP data types

--- webapp/model/sapType.js
+++ webapp/model/sapType.js
@@ -1,38 +1,41 @@
-sap.ui.define([], function() {
+sap.ui.define([
+	"sap/ui/model/type/Float",
+	"sap/ui/model/type/Integer",
+	"sap/ui/model/type/String"
+], function(Float) {
 	"use strict";
 
 	return {
-		
-		
 
-		/**
-		 * Rounds the number unit value to 2 digits
-		 * @public
-		 * @param {string} sValue the number string to be rounded
-		 * @returns {string} sValue with 2 digits rounded
-		 */
-		numberUnit: function(sValue) {
-			if (!sValue) {
-				return "";
-			}
-			return parseFloat(sValue).toFixed(2);
-		},
-		/**
-		 * Defines a value state based on the stock level
-		 *
-		 * @public
-		 * @param {number} iValue the stock level of a product
-		 * @returns {string} sValue the state for the stock level
-		 */
-		quantityState: function(iValue) {
-			if (iValue === 0) {
-				return "Error";
-			} else if (iValue <= 10) {
-				return "Warning";
-			} else {
-				return "Success";
-			}
-		}
+		QUAN: new sap.ui.model.type.Float({
+			minIntegerDigits: 1, // minimal number of non-fraction digits
+			maxIntegerDigits: 12, // maximal number of non-fraction digits
+			minFractionDigits: 3, // minimal number of fraction digits
+			maxFractionDigits: 3, // maximal number of fraction digits
+			emptyString: null // defines what empty string is parsed as and what is formatted as empty string
+		}),
+
+		LENUM: new sap.ui.model.type.Integer({
+			minIntegerDigits: 1, // minimal number of non-fraction digits
+			maxIntegerDigits: 20, // maximal number of non-fraction digits
+			emptyString: null // defines what empty string is parsed as and what is formatted as empty string
+		}),
+
+		LGORT: new sap.ui.model.type.String(null, {
+			minLength: 4,
+			maxLength: 4
+		}),
+
+		AUFNR: new sap.ui.model.type.String(null, {
+			minLength: 7,
+			maxLength: 7,
+			startsWith: "1"
+		}),
+
+		MEINS: new sap.ui.model.type.String(null, {
+			minLength: 1,
+			maxLength: 4
+		})
 
 	};
 
