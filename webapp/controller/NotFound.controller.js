c576347896dee207b07172791fcdd6853ffb09ef
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2017-11-28 13:05:59 +0000

    Reformatting

--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -1,19 +1,18 @@
 sap.ui.define([
-		"com/mii/scanner/controller/BaseController"
-	], function (BaseController) {
-		"use strict";
+	"com/mii/scanner/controller/BaseController"
+], function(BaseController) {
+	"use strict";
 
-		return BaseController.extend("com.mii.scanner.controller.NotFound", {
+	return BaseController.extend("com.mii.scanner.controller.NotFound", {
 
-			/**
-			 * Navigates to the startpage when the link is pressed
-			 * @public
-			 */
-			onLinkPressed : function () {
-				this.getRouter().navTo("home");
-			}
+		/**
+		 * Navigates to the startpage when the link is pressed
+		 * @public
+		 */
+		onLinkPressed: function() {
+			this.getRouter().navTo("home");
+		}
 
-		});
+	});
 
-	}
-);
+});
