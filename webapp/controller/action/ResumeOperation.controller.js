a2627839ae35de8c1fb8c84693c44127be5be57a
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2018-01-06 14:49:46 +0000

    Switch operation action pages to actual controllers Instead of dummy controller class

---
+++ webapp/controller/action/ResumeOperation.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+	"./ActionBaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox",
+	"com/mii/scanner/model/sapType"
+], function(ActionBaseController, JSONModel, MessageBox, sapType) {
+	"use strict";
+
+	return ActionBaseController.extend("com.mii.scanner.controller.action.ResumeOperation", {
+
+		sapType: sapType,
+
+		_oInitData: {
+
+		},
+
+		onInit: function() {
+			var oModel = new JSONModel(),
+				oData = jQuery.extend({}, this._oInitData);
+			oModel.setData(oData);
+			this.setModel(oModel);
+		}
+	});
+
+});
