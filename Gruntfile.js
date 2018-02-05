5185d444f9dff87c0eeb5050123cf36f634fce45
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2018-02-05 21:30:10 +0000

    [INTERNAL] Optimize Grunt build
    Remove unnecessary tasks
    Remove test folders from dist by using .project.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,12 @@
 module.exports = function(grunt) {
 	'use strict';
 
-	var config = {
+	var oConfig = {
 		"openui5_preload": {
 			"preloadTmp": {
 				"options": {
-					"resources": {
-						"cwd": "dist/tmp",
-						"src": [
-							"**/*.js",
-							"**/*.fragment.html",
-							"**/*.fragment.json",
-							"**/*.fragment.xml",
-							"**/*.view.html",
-							"**/*.view.json",
-							"**/*.view.xml",
-							"**/*.properties",
-							"manifest.json",
-							"!test/**",
-							"!**/node_modules/**",
-							"!neo-app.json"
-						],
-						"prefix": "com/mii/scanner"
-					},
-					"compress": true,
-					"dest": "dist/tmp"
-				},
-				"components": true
+					"compress": true
+				}
 			}
 		},
 		replace: {
@@ -57,83 +37,7 @@ module.exports = function(grunt) {
 				}]
 			}
 		},
-		uglify: {
-			dist: {
-				files: [{
-					expand: true,
-					src: ['dist/controller/**/*.js', '!dist/controller/**/*.min.js', '!dist/controller/**/*-dbg*.js'],
-					dest: 'dist/controller',
-					cwd: '.',
-					rename: function(dst, src) {
-						grunt.log.writeln("Uglify " + src);
-						// To keep the source js files and make new files as `*.min.js`:
-						// return dst + '/' + src.replace('.js', '.min.js');
-						// Or to override to src:
-						return src;
-					}
-				}, {
-					expand: true,
-					src: ['dist/model/**/*.js', '!dist/model/**/*.min.js', '!dist/model/**/*-dbg*.js'],
-					dest: 'dist/model',
-					cwd: '.',
-					rename: function(dst, src) {
-						grunt.log.writeln("Uglify " + src);
-						// To keep the source js files and make new files as `*.min.js`:
-						// return dst + '/' + src.replace('.js', '.min.js');
-						// Or to override to src:
-						return src;
-					}
-				}, {
-					expand: true,
-					src: ['dist/Component.js'],
-					dest: 'dist',
-					cwd: '.',
-					rename: function(dst, src) {
-						grunt.log.writeln("Uglify " + src);
-						// To keep the source js files and make new files as `*.min.js`:
-						// return dst + '/' + src.replace('.js', '.min.js');
-						// Or to override to src:
-						return src;
-					}
-				}]
-			}
-		},
-		xmlmin: {
-			dist: {
-				options: {
-					preserveComments: false
-				},
-				files: {
-					'dist/view/App.view.xml': 'dist/view/App.view.xml',
-					'dist/view/Login.view.xml': 'dist/view/Login.view.xml',
-					'dist/view/NotFound.view.xml': 'dist/view/NotFound.view.xml',
-					'dist/view/nav/Home.view.xml': 'dist/view/nav/Home.view.xml',
-					'dist/view/nav/GoodsMovement.view.xml': 'dist/view/nav/GoodsMovement.view.xml',
-					'dist/view/nav/StatusChange.view.xml': 'dist/view/nav/StatusChange.view.xml',
-					'dist/view/action/fragments/StorageUnitInfoForm.fragment.xml': 'dist/view/action/fragments/StorageUnitInfoForm.fragment.xml',
-					'dist/view/action/GoodsReceipt.view.xml': 'dist/view/action/GoodsReceipt.view.xml',
-					'dist/view/action/GoodsIssue.view.xml': 'dist/view/action/GoodsIssue.view.xml',
-					'dist/view/action/FinishOperation.view.xml': 'dist/view/action/FinishOperation.view.xml',
-					'dist/view/action/InterruptOperation.view.xml': 'dist/view/action/InterruptOperation.view.xml',
-					'dist/view/action/ResumeOperation.view.xml': 'dist/view/action/ResumeOperation.view.xml',
-					'dist/view/action/Startperation.view.xml': 'dist/view/action/StartOperation.view.xml'
-				}
-			}
-		},
 		copy: {
-			test: {
-				files: [{
-					"expand": true,
-					"src": "test/**",
-					"dest": "dist",
-					"cwd": "webapp"
-				}, {
-					"expand": true,
-					"src": "test.html",
-					"dest": "dist",
-					"cwd": "webapp"
-				}]
-			},
 			irpt: {
 				files: [{
 					"expand": true,
@@ -148,23 +52,16 @@ module.exports = function(grunt) {
 		}
 	};
 
-	grunt.file.write("GruntConfigInitial.log", JSON.stringify(grunt.config(), null, 2));
-
 	grunt.loadNpmTasks('@sap/grunt-sapui5-bestpractice-build');
-	grunt.loadNpmTasks('grunt-text-replace');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.config.merge(config);
+	grunt.config.merge(oConfig);
 
 	grunt.registerTask('default', [
-		//'replace:version',
 		'lint',
 		'clean',
 		'build',
 		'copy:test',
 		'copy:irpt'
 	]);
-
-	grunt.file.write("GruntConfigFinal.log", JSON.stringify(grunt.config(), null, 2));
 
 };
