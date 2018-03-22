c1e4f227240343ad4aee08b9f0b223a3289d5c8c
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2018-03-22 15:23:46 +0000

    [INTERNAL] (device): add defaults to device model to provide device-specific values

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,23 +1,41 @@
 sap.ui.define([
-		"sap/ui/model/json/JSONModel",
-		"sap/ui/Device"
-	], function (JSONModel, Device) {
-		"use strict";
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+], function(JSONModel, Device) {
+	"use strict";
 
-		return {
+	return {
 
-			createDeviceModel : function () {
-				var oModel = new JSONModel(Device);
-				oModel.setDefaultBindingMode("OneWay");
-				return oModel;
-			},
-			
-			prepareUserModel : function () {
-				var oModel = new JSONModel();
-				return oModel;
-			}
+		createDeviceModel: function() {
+			var oModel = new JSONModel(Device),
+				oDefaults;
+			oModel.setDefaultBindingMode("OneWay");
 
-		};
+			oDefaults = {
+				clearQuantityInputIcon: {
+					mobile: {
+						size: '1.75rem',
+						width: '1.75rem',
+						height: '2.75rem'
+					},
+					desktop: {
+						size: '1.25rem',
+						width: '1.25rem',
+						height: '2.00rem'
+					}
+				}
+			};
 
-	}
-);
+			oModel.setProperty("/defaults", oDefaults);
+
+			return oModel;
+		},
+
+		prepareUserModel: function() {
+			var oModel = new JSONModel();
+			return oModel;
+		}
+
+	};
+
+});
