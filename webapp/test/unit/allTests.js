87ca888f6b393ee21b5c342025b185ac42027490
Gembe Philipp <philipp.gembe@gembe-consulting.de>
2018-05-26 12:22:16 +0000

    [TEST] move files to gm subfolder

--- webapp/test/unit/allTests.js
+++ webapp/test/unit/allTests.js
@@ -1,13 +1,13 @@
 sap.ui.define([
 	"test/unit/model/sapType",
 	"test/unit/model/formatter",
-	"test/unit/controller/ActionBaseController",
-	"test/unit/controller/Login.controller",
-	"test/unit/controller/GoodsReceipt.controller",
-	"test/unit/controller/RollerConveyor.controller",
-	"test/unit/controller/BaseController",
 	"test/unit/helper/Utilities",
 	"test/unit/helper/UserHandler",
+	"test/unit/controller/BaseController",
+	"test/unit/controller/ActionBaseController",
+	"test/unit/controller/Login.controller",
+	"test/unit/controller/gm/GoodsReceipt.controller",
+	"test/unit/controller/gm/RollerConveyor.controller",
 	"test/unit/controller/gm/GoodsIssueController"
 ], function() {
 	"use strict";
